5de8954841e531960b3e2bf4f8e2ead0a80b4631
NicoDora <pjh76574734@gmail.com>
2025-10-20 20:25:40 +0900

    modify/#3: DI 토큰을 Symbol을 사용하도록 수정

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,4 +1,8 @@
 import { Module } from "@nestjs/common";
+import {
+  BCRYPT_PASSWORD_HASHER_DI_TOKEN,
+  CLASS_VALIDATOR_EMAIL_VALIDATOR_DI_TOKEN,
+} from "@src/member/constants/di-tokens";
 import { BcryptPasswordHasher } from "@src/member/infrastructure/security/bcrypt.password-hasher";
 import { ClassValidatorEmailValidator } from "@src/member/infrastructure/validators/class-validator.email.validator";
 
@@ -6,8 +10,14 @@ import { ClassValidatorEmailValidator } from "@src/member/infrastructure/validat
   imports: [],
   controllers: [],
   providers: [
-    { provide: "IEmailValidator", useClass: ClassValidatorEmailValidator },
-    { provide: "IPasswordHasher", useClass: BcryptPasswordHasher },
+    {
+      provide: CLASS_VALIDATOR_EMAIL_VALIDATOR_DI_TOKEN,
+      useClass: ClassValidatorEmailValidator,
+    },
+    {
+      provide: BCRYPT_PASSWORD_HASHER_DI_TOKEN,
+      useClass: BcryptPasswordHasher,
+    },
   ],
 })
 export class MemberModule {}
