bd0c5928d0c0976d4a14bde0beee946c4ce080f4
NicoDora <pjh76574734@gmail.com>
2025-10-21 15:52:33 +0900

    feat/#3: User Entity signUp 메서드 구현

--- src/member/domain/aggregate/user.entity.ts
+++ src/member/domain/aggregate/user.entity.ts
@@ -1,17 +1,54 @@
 import { AuthenticationInfo } from "@src/member/domain/value-objects/authentication-info.vo";
+import { Role, RoleEnum } from "@src/member/domain/value-objects/role.vo";
+import { UserProfile } from "@src/member/domain/value-objects/user-profile.vo";
+
+interface SignUpParams {
+  id: string;
+  authInfo: AuthenticationInfo;
+  profile: UserProfile;
+}
 
 /**
  * User Aggregate Root (User Entity)
  * @description 이 클래스를 통해서만 User 도메인의 상태를 변경할 수 있습니다.
- * @author NicoDora
  */
 export class User {
+  public readonly id: string;
+  private authInfo: AuthenticationInfo;
+  private profile: UserProfile;
+  private role: Role;
+  private createdAt: Date;
+  private updatedAt: Date;
+
   private constructor(
-    public readonly id: string,
-    private authInfo: AuthenticationInfo,
-    private profile: UserProfile,
-    private role: UserRole,
-    private createdAt: Date,
-    private updatedAt: Date,
-  ) {}
+    id: string,
+    authInfo: AuthenticationInfo,
+    profile: UserProfile,
+    role: Role,
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    this.id = id;
+    this.authInfo = authInfo;
+    this.profile = profile;
+    this.role = role;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+  public static signUp(params: SignUpParams): User {
+    const now = new Date();
+    const defaultRole = Role.create(RoleEnum.USER);
+
+    const user = new User(
+      params.id,
+      params.authInfo,
+      params.profile,
+      defaultRole,
+      now,
+      now,
+    );
+
+    return user;
+  }
 }
