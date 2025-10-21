54d92e0cd9a541e113ae85bbc89e240827757513
NicoDora <pjh76574734@gmail.com>
2025-10-21 15:01:28 +0900

    feat/#3: Role VO 생성

---
+++ src/member/domain/value-objects/role.vo.ts
@@ -0,0 +1,22 @@
+enum RoleEnum {
+  USER = "USER",
+  ADMIN = "ADMIN",
+}
+
+export class Role {
+  public readonly value: RoleEnum;
+
+  private constructor(value: RoleEnum) {
+    this.value = value;
+  }
+
+  public static create(role: string): Role {
+    const upperRole = role.toUpperCase();
+
+    if (!(upperRole in RoleEnum)) {
+      throw new Error(`${role}는 지원하지 않는 역할입니다.`);
+    }
+
+    return new Role(upperRole as RoleEnum);
+  }
+}
