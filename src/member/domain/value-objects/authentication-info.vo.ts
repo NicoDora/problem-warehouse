449a380b76610f73129d9ffd7d2eee618528b4e7
NicoDora <pjh76574734@gmail.com>
2025-10-21 14:14:57 +0900

    feat/#3: AuthenticationInfo VO create 메서드 추가

--- src/member/domain/value-objects/authentication-info.vo.ts
+++ src/member/domain/value-objects/authentication-info.vo.ts
@@ -1,11 +1,13 @@
+import { IEmailValidator } from "@src/member/domain/ports/email.validator.interface";
+import { IPasswordHasher } from "@src/member/domain/ports/password-hasher.interface";
 import { Email } from "@src/member/domain/value-objects/email.vo";
 import { HashedPassword } from "@src/member/domain/value-objects/hashed-password.vo";
 import { LoginProvider } from "@src/member/domain/value-objects/login-provider.vo";
 
 export class AuthenticationInfo {
-  readonly email: Email;
-  readonly password: HashedPassword | null;
-  readonly provider: LoginProvider;
+  public readonly email: Email;
+  public readonly password: HashedPassword | null;
+  public readonly provider: LoginProvider;
 
   private constructor(
     email: Email,
@@ -15,5 +17,27 @@ export class AuthenticationInfo {
     this.email = email;
     this.password = password;
     this.provider = provider;
+  }
+
+  public static async create(
+    emailString: string,
+    plainPassword: string | null,
+    providerString: string,
+    emailValidator: IEmailValidator,
+    passwordHasher: IPasswordHasher,
+  ): Promise<AuthenticationInfo> {
+    const email = Email.create(emailString, emailValidator);
+    const provider = LoginProvider.create(providerString);
+    let password: HashedPassword | null = null;
+
+    if (provider.isLocal()) {
+      if (!plainPassword) {
+        throw new Error("로컬 가입 시에는 비밀번호가 필요합니다.");
+      }
+
+      password = await HashedPassword.create(plainPassword, passwordHasher);
+    }
+
+    return new AuthenticationInfo(email, password, provider);
   }
 }
