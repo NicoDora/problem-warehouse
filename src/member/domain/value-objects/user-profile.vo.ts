1614709cfb99df0b53d30c8d1d310b588b4a9997
NicoDora <pjh76574734@gmail.com>
2025-10-21 14:55:13 +0900

    feat/#3: UserProfile VO 생성

--- src/member/domain/value-objects/user-profile.vo.ts
+++ src/member/domain/value-objects/user-profile.vo.ts
@@ -1,4 +1,19 @@
+import { Nickname } from "@src/member/domain/value-objects/nickname.vo";
+
 export class UserProfile {
   public readonly nickname: Nickname;
   public readonly imageUrl: string | null;
+
+  private constructor(nickname: Nickname, imageUrl: string | null) {
+    this.nickname = nickname;
+    this.imageUrl = imageUrl;
+  }
+
+  public static create(
+    nicknameString: string,
+    imageUrl?: string | null,
+  ): UserProfile {
+    const nickname = Nickname.create(nicknameString);
+    return new UserProfile(nickname, imageUrl ?? null);
+  }
 }
